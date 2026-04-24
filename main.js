20ab9452d7f841f8d26a13c54393b16c91c0de82
Pyrad-Time <marcos0fataak@gmail.com>
2026-04-24 09:06:07 -0300

    feat(filter): implement task status filter

--- main.js
+++ main.js
@@ -2,11 +2,15 @@
 const list = document.getElementById('task-list')
 const input = document.getElementById('search')
 const status = document.getElementById('status')
+const button = document.getElementsByTagName('button')
+
+const btn = [...button]
 
 
 let state = {
     current: '',
     tasks: [],
+    status: 'all'
 }
 
 
@@ -17,18 +21,53 @@ fetch('https://jsonplaceholder.typicode.com/todos')
     .then(render)
 
 
+
+function showTask(e) {
+    const li = document.createElement('li')
+    const p = document.createElement('p')
+
+    li.textContent = e.title
+    p.textContent = e.completed
+    list.append(li)   
+    li.append(p)
+}
+
 function render() {
     list.textContent = ''
+
     state.tasks.forEach(e => {
-        const li = document.createElement('li')
-        li.textContent = e.id
-        list.append(li)
-           
+        
+        if (e.completed === true) {
+            if(state.status === "completed") {
+                showTask(e)
+            }
+
+        }
+
+        if (!e.completed) {
+            if(state.status === "pending") {
+                showTask(e)
+            }
+        }
+
+        if(state.status === "all")
+            showTask(e)
+
     })
     
 }
+
+
+btn.forEach(e => {
+    e.addEventListener("click", (e) => {
+        const data = e.target.dataset.filter
+        state.status = data
+        render()
+    })
+})
 
 input.addEventListener("keypress", (e) => {
     state.current = Number(state.current + e.key)
 
 })
+
