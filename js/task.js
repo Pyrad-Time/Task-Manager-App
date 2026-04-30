95024df57758b58f74da45c1860f7937875f26e0
Pyrad-Time <marcos0fataak@gmail.com>
2026-04-30 10:01:01 -0300

    feat(task): implement task toggle, delete and edit button structure

--- js/task.js
+++ js/task.js
@@ -1,9 +1,23 @@
 export function showTask(e, list) {
+    const fragment = document.createDocumentFragment()
     const li = document.createElement('li')
     const p = document.createElement('p')
+    const btnEdit = document.createElement('button')
+    const btnDel = document.createElement('button')
+    const inputCheck = document.createElement('input')
 
-    li.textContent = e.title
-    p.textContent = e.completed
+    p.textContent = e.title
+    btnEdit.textContent = "Edit"
+    btnDel.textContent = "Delete"
+    inputCheck.type = "checkbox"
+
+
+    li.className = "task"
+    btnEdit.className = "btnTask"
+    btnDel.className = "btnTask"
+    
     list.append(li)   
+    li.append(inputCheck)
     li.append(p)
+    li.append(btnEdit, btnDel)
 }
